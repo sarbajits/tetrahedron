92351929a7cad8cac70c07dce78f4cb255fbc490
sarbajits <sarbajitsahoo8260@gmail.com>
2025-07-24 18:59:18 +0530

    404.html and scroll to top and change all img location to public

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Academics from './pages/Academics';
 import Facilities from './pages/Facilities';
 import Gallery from './pages/Gallery';
 import Contact from './pages/Contact';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <main className="flex-grow">
+          <ScrollToTop />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
